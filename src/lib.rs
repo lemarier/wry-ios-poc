d4fbbaf99cd6dcc3ab5ff4b3c8b4b4d0eb718d2c
David Lemarier <david@lemarier.ca>
2021-04-25 12:59:28 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use cacao::ios::{
     App, AppDelegate, Scene, SceneConfig, SceneConnectionOptions, SceneSession, Window,
     WindowSceneDelegate,
 };
-use cacao::layout::{Layout, LayoutConstraint};
+use cacao::{layout::{Layout, LayoutConstraint}, webview::InjectAt};
 use cacao::view::{View, ViewController, ViewDelegate};
 use cacao::webview::{WebView, WebViewConfig, WebViewDelegate};
 use mobile_entry_point::mobile_entry_point;
@@ -25,7 +25,49 @@ impl AppDelegate for TestApp {
 #[derive(Default)]
 pub struct WebViewInstance;
 
-impl WebViewDelegate for WebViewInstance {}
+impl WebViewDelegate for WebViewInstance {
+    fn on_message(&self, name: &str, body: &str) {
+        println!("name: {}, body: {}", name, body);
+    }
+
+    fn on_custom_protocol_request(&self, path: &str) -> Option<Vec<u8>> {
+
+        let requested_asset_path = path.replace("wry.dev://", "");
+
+        let index_html = r#"
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
+            <body style="background-color: #7FFFD4">
+            <h1>Welcome 🍫</h1>
+            <a href="/hello.html">Link</a>
+            </body>
+        </html>"#; 
+        
+        let link_html = r#"
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
+            <body>
+            <h1>Hello!</h1>
+            <a href="/">Back home</a>
+            </body>
+        </html>"#; 
+
+        return match requested_asset_path.as_str() {
+            "/hello.html" => Some(link_html.as_bytes().into()),
+            _ => Some(index_html.as_bytes().into()),
+        }
+    }
+}
 
 pub struct RootView {
     pub webview: WebView<WebViewInstance>,
@@ -33,8 +75,21 @@ pub struct RootView {
 
 impl RootView {
     pub fn new() -> Self {
+        let mut config = WebViewConfig::default();
+
+        config.add_handler("notify");
+        config.add_custom_protocol("wry.dev");
+
+        let script = r#"
+            window.addEventListener('DOMContentLoaded', (event) => {
+                webkit.messageHandlers.notify.postMessage("hello");
+            });
+        "#;
+
+       config.add_user_script(script, InjectAt::Start, true);
+        
         RootView {
-            webview: WebView::with(WebViewConfig::default(), WebViewInstance::default()),
+            webview: WebView::with(config, WebViewInstance::default()),
         }
     }
 
@@ -46,9 +101,10 @@ impl RootView {
 impl ViewDelegate for RootView {
     const NAME: &'static str = "RootView";
 
+
     fn did_load(&mut self, view: View) {
         view.add_subview(&self.webview);
-        self.load_url("https://tauri.studio/");
+        self.load_url("wry.dev://");
         LayoutConstraint::activate(&[
             self.webview.top.constraint_equal_to(&view.top),
             self.webview
@@ -70,6 +126,7 @@ pub struct WindowScene {
 }
 
 impl WindowSceneDelegate for WindowScene {
+
     fn will_connect(&self, scene: Scene, session: SceneSession, options: SceneConnectionOptions) {
         let bounds = scene.get_bounds();
         let mut window = Window::new(bounds);
